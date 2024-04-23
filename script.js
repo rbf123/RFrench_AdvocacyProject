5847bd5c4ee17f06aeec0e5a460100d8aa7340fb
Romina French <108244092+rbf123@users.noreply.github.com>
2024-04-23 20:04:31 +0000

    Added information for texas, online in the USA, california, florida and new york

--- script.js
+++ script.js
@@ -244,3 +244,19 @@ reduceMotionBtn.addEventListener('click', reduceMotion);
 
 window.addEventListener('scroll', reveal);
 //end of reducemotion
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
